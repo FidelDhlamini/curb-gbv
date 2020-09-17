e01dfd2e3ab5567f66e0c45a5e168231dd4de0c7
null <null>
2020-09-17 11:20:27 +0200

    added fidels code

--- app/src/main/java/services/Report.java
+++ app/src/main/java/services/Report.java
@@ -1,21 +1,21 @@
 package main.java.services;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Report {
 
-    public void takeUserDetails(String name,String email,String location,String details,String victimState){
-        Map<String,Object> user = new HashMap<>();
-        user.put("email",email);
-        user.put("name",name);
-        user.put("location",location);
-        user.put("details",details);
-        user.put("victimState",victimState);
 
+    public Map<String, Object> takeUserDetails(String name, String email, String location, String details, String victimState) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", email);
+        user.put("name", name);
+        user.put("location", location);
+        user.put("details", details);
+        user.put("victimState", victimState);
+
+        return user;
     }
-
-
-
-
 }
+
+
